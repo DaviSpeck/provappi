e85c9798d61053a92ea0e6b1e9377d82fa443c6e
daviwalkr <davi.speck@walkr.app>
2022-05-10 17:18:15 -0300

    Atualizando index

--- registro_academico/views.py
+++ registro_academico/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from registro_academico.models import Senha, Categoria, StatusSenha, Tipo
 
 def index(request):
+
 	return render(request, 'index.html', {'senhas': Senha.objects.all()})
 
